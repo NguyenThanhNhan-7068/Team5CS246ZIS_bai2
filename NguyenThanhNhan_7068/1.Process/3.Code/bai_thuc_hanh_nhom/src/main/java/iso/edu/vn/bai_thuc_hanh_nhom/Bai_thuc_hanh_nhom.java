a1c85f07580cfddcd79cf9214dcc3ab7ae62a287
NguyenThanhNhan-7068 <162886633+NguyenThanhNhan-7068@users.noreply.github.com>
2024-03-22 20:16:37 +0700

    update code
    cập nhật code giao diện dự án

---
+++ NguyenThanhNhan_7068/1.Process/3.Code/bai_thuc_hanh_nhom/src/main/java/iso/edu/vn/bai_thuc_hanh_nhom/Bai_thuc_hanh_nhom.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package iso.edu.vn.bai_thuc_hanh_nhom;
+
+/**
+ *
+ * @author MSI
+ */
+public class Bai_thuc_hanh_nhom {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
